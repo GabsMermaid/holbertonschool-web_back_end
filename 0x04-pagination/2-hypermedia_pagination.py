1d9ebc058d54b721399a9e54215bb1a7a904510d
GabsMermaid <g_martinez_rodz@gmail.com>
2022-06-14 13:36:21 +0000

    Fixing pycode style

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-""" Implement a get_hyper method that takes the same arguments (and defaults) as get_page and
-returns a dictionary containing the following key-value pairs """
+""" Implement a get_hyper method that takes the same arguments (and defaults)
+as get_page and returns a dictionary containing the
+following key-value pairs """
 
 import csv
 import math
